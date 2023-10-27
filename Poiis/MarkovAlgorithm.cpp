4ee1f4609202a2596e569f269f43157df665ec6e
Екатерина <ekaterina16122004@gmail.com>
2023-10-27 13:42:08 +0300

    laba 1

---
+++ Poiis/MarkovAlgorithm.cpp
@@ -0,0 +1,52 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+class Rule {
+private:
+    string from;
+    string replacement;
+public:
+
+    Rule(const string& from, const string& replacement) : from(from), replacement(replacement) {}
+
+    string apply(const string& input) const {
+        string output = input;
+        size_t pos = output.find(from);
+        if (pos != string::npos) {
+            return output.replace(pos, from.size(), replacement);
+        }
+        return output;
+    }
+
+};
+
+class MarkovAlgorithm {
+private:
+    vector<Rule> rules;
+
+public:
+    MarkovAlgorithm(const vector<Rule>& rules) : rules(rules) {}
+
+    string apply(const string& input) {
+        string result = input;
+        bool changed;
+        do {
+            changed = false;
+            for (const auto& rule : rules) {
+                    string old = result;
+                    result = rule.apply(result);
+                    
+                    changed = old != result;
+            }
+
+        } while (changed);
+
+        return result;
+    }
+
+};
+
+
---
+++ UnitTest1/MarkovAlgorithmTest.cpp
@@ -0,0 +1,69 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\Poiis\MarkovAlgorithm.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		
+        TEST_METHOD(TestMethod2) {
+            vector<Rule> rules
+            {
+                Rule("aa", "bb"),
+                    Rule("bb", "c"),
+                    Rule("c", "")
+            };
+            MarkovAlgorithm ma(rules);
+
+            // Задаем входные данные и ожидаемый результат
+            string input = "abaab";
+            string expectedOutput = "a";
+
+            // Применяем правила к входным данным
+            string actualOutput = ma.apply(input);
+
+            // Проверяем, что фактический результат совпадает с ожидаемым
+            Assert:: AreEqual(actualOutput, expectedOutput);
+        }
+        TEST_METHOD(TestMethod3) {
+            vector<Rule> rules;
+            MarkovAlgorithm ma(rules);
+
+            // Задаем входные данные и ожидаемый результат
+            string input = "A";
+            string expectedOutput = "A";
+
+            // Применяем правила к входным данным
+            string actualOutput = ma.apply(input);
+
+            // Проверяем, что фактический результат совпадает с ожидаемым
+            Assert::AreEqual(actualOutput, expectedOutput);
+        }
+        TEST_METHOD(TestMethod1)
+        {
+            vector<Rule> rules
+            {
+                Rule ("A", "B"),
+                Rule ("B", "C"),
+                Rule ("C", "D")
+            };
+            MarkovAlgorithm algorithm(rules);
+
+            // Задаем входные данные и ожидаемый результат
+            string input = "ACACAC";
+            string expectedOutput = "DDDDDD";
+
+            // Применяем правила к входным данным
+            string actualOutput = algorithm.apply(input);
+
+            // Проверяем, что фактический результат совпадает с ожидаемым
+            Assert::AreEqual(actualOutput, expectedOutput);
+        }
+
+	};
+}
